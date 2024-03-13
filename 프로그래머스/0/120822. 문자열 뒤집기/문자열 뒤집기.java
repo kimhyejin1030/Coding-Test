f4944b2c62808e01a5408b3ac18cc1a2c06bfb21
KIMHYEJIN <k.hyejin1030@gmail.com>
2024-03-13 10:41:30 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.11 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
         
-        StringBuffer sb = new StringBuffer(answer);
+        StringBuffer sb = new StringBuffer(my_string);
         String reverse = sb.reverse().toString();
         
         return reverse;
