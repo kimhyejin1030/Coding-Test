6f2f95d47420efdf5825bbc0d7af9c5f408d9217
KIMHYEJIN <k.hyejin1030@gmail.com>
2024-03-21 23:35:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 0.15 ms
+메모리: 75.9 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 22일 16:10:42
+2024년 03월 21일 23:35:30
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,19 +1,21 @@
 class Solution {
+    public int GCD(int num1, int num2) {
+        if (num1 % num2 == 0)
+            return num2;
+        return GCD(num2, num1 % num2);
+    }
+
     public int[] solution(int denum1, int num1, int denum2, int num2) {
-        int[] answer = new int[2]; 
-        
-        int n = num1*num2; //분모
-        int de = denum1*num2+denum2*num1; //분자
-        int max = 1;  
-        
-        for(int i=1; i <= n && i<= de; i++){     //최대 공약수
-            if(n%i==0 && de%i==0){
-                max = i;
-            }
-        }
-        answer[0] = de/max; //분자
-        answer[1] = n/max; //분모
-        
+
+        denum1 *= num2;
+        denum2 *= num1;
+
+        int[] answer = new int[]{denum1 + denum2, num1 * num2};
+
+        int max = GCD(answer[0], answer[1]);
+        answer[0] /= max;
+        answer[1] /= max;
+
         return answer;
     }
 }
