b886db7f2d7227d159502f22900b932646518de9
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-03-20 17:21:39 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.06 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -2,9 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1; i<=n; i++){
+        for(int i=1; i*i<=n; i++){
             if(n%i==0){
                 answer++;
+                if(i != n/i) answer++;
             }
         }
         return answer;
