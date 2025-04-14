3043af2a4e4ae5ca0d4d9bb33e4b53e40158c6fb
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-04-14 20:59:22 +0900

    [level 0] Title: 짝수의 합, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -3,8 +3,9 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<=n; i++){
-            if(i%2==0)
+            if(i % 2 == 0) {
                 answer += i;
+            }
         }
         return answer;
     }
