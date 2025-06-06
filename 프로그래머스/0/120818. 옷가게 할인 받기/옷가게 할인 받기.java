c0d451eb09a73cdcd9b77b83592b2de8dc168cf4
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-06-06 13:03:13 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,18 +1,8 @@
 class Solution {
     public int solution(int price) {
-        int answer = 0;
-        
-        if( price >= 500000) {
-            answer = (int)(price*0.8);
-        } else if( price >= 300000){
-            answer = (int)(price * 0.9);
-        }else if (price >= 100000){
-            answer = (int)(price * 0.95);
-        } else {
-            answer = price;
-        }
-        
-        
-        return answer;
+        return price >= 500000? price * 80 / 100:
+               price >= 300000? price * 90 / 100:
+               price >= 100000? price * 95 / 100:
+               price;
     }
 }
