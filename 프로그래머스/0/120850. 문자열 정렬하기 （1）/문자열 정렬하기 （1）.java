8ea672c55cadfdf3a0dc5b45313f69bb198237a0
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-07-15 21:05:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.52 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String result = my_string.replaceAll("[a-z]","");
+        
+        int[] answer = new int[result.length()];
+        for(int i=0; i < result.length(); i++) {
+            answer[i] = Character.getNumericValue(result.charAt(i));
+        }
+        Arrays.toString(answer);
+        Arrays.sort(answer);
+        return answer;
+    }
+}
