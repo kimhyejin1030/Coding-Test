3c629def6a44435f012341cb2dff23f3377fce82
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-07-15 16:57:54 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,6 +1,10 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replaceAll("[aeiou]","");
+        String answer = my_string.replace("a","")
+                           .replace("e","")
+                           .replace("i","")
+                           .replace("o","")
+                           .replace("u","");
         return answer;
     }
 }
