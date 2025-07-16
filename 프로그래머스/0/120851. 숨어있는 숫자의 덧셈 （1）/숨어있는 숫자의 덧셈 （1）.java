b9043093c8c2a49b7298c4cf78fdc87b1743a085
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-07-16 12:24:01 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.32 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(String my_string) {
         
-        String str = my_string.replaceAll("[a-z]","");
-        int answer = str.chars()
-            .filter(Character::isDigit)
-            .map(ch -> ch - '0')
-            .sum();
+        String str = my_string.replaceAll("[^0-9]","");
+        
+        int answer = 0;
+        for(char ch : str.toCharArray()) {
+            answer += ch - '0';
+        }
         
         return answer;
     }
