ffb505f67c1745a9eccfd2932d700b219f8a6cb2
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-09-03 11:13:50 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.17 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,16 +1,16 @@
 class Solution {
     public long solution(String numbers) {
         
-        numbers = numbers.replace("zero","0");
-        numbers = numbers.replace("one","1");
-        numbers = numbers.replace("two","2");
-        numbers = numbers.replace("three","3");
-        numbers = numbers.replace("four","4");
-        numbers = numbers.replace("five","5");
-        numbers = numbers.replace("six","6");
-        numbers = numbers.replace("seven","7");
-        numbers = numbers.replace("eight","8");
-        numbers = numbers.replace("nine","9");
+        numbers = numbers.replace("zero","0")
+                         .replace("one","1")
+                         .replace("two","2")
+                         .replace("three","3")
+                         .replace("four","4")
+                         .replace("five","5")
+                         .replace("six","6")
+                         .replace("seven","7")
+                         .replace("eight","8")
+                         .replace("nine","9");
         
         long answer = Long.parseLong(numbers);
         
