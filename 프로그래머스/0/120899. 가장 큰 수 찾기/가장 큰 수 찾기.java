1f6913a516fb690cff28f566d82c99ab4b7e7a45
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-09-12 22:44:43 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] array) {
+        
+        int max = array[0];
+        int index = 0;
+        
+        for(int i=0; i < array.length; i++){
+            if(array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+        
+        int[] answer = new int[]{max, index};
+        
+        return answer;
+    }
+}
