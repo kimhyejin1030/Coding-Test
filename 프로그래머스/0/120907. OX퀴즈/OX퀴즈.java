ae0cbace42864e9a1c787e360f17754b9ecefa7d
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-11-12 21:50:00 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,30 +1,17 @@
-import java.util.Arrays;
-
 public class Solution {
-    public static String[] solution(String[] quiz) {
+    public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
 
         for (int i = 0; i < quiz.length; i++) {
-            String[] parts = quiz[i].split(" "); // ["3", "+", "4", "=", "7"]
+            String[] parts = quiz[i].split(" "); // "3 + 4 = 7" → ["3", "+", "4", "=", "7"]
             int x = Integer.parseInt(parts[0]);
             int y = Integer.parseInt(parts[2]);
             int z = Integer.parseInt(parts[4]);
-            
-            int result = 0;
-            if (parts[1].equals("+")) {
-                result = x + y;
-            } else if (parts[1].equals("-")) {
-                result = x - y;
-            }
 
+            int result = (parts[1].equals("+")) ? x + y : x - y;
             answer[i] = (result == z) ? "O" : "X";
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        String[] quiz = {"3 + 4 = 7", "5 - 2 = 5", "10 - 7 = 3"};
-        System.out.println(Arrays.toString(solution(quiz))); 
     }
 }
