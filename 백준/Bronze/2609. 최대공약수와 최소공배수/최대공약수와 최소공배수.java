7d3835c0463e083ec59095a65f5e8abc31c3ed9d
KIMHYEJIN <k.hyejin1030@gmail.com>
2025-12-19 08:50:31 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = 0;
+
+        for (int i = 1; i <= Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        
+        int lcm = (a * b) / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
